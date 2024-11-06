103d11279c1f5f77022c7a32c023078d146d2f3c
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2024-11-06 11:51:16 -0500

    styling stuff

--- src/app/chartjslinechart/page.tsx
+++ src/app/chartjslinechart/page.tsx
@@ -1,12 +1,16 @@
-'use client';
 import ChartJsLineChart from './_components/ChartJsLineChart';
-export default function ChartJs() {
+
+import './page.css';
+
+const ChartJs = () => {
   return (
     <>
-      <div style={{ width: '100%', margin: '0 auto', textAlign: 'center' }}>
+      <div className='title-wrapper'>
         <h1>Chart JS Line Chart</h1>
       </div>
       <ChartJsLineChart />
     </>
   );
-}
+};
+
+export default ChartJs;
